55b13c8c3b9bc7019b804904dd9ed7d1925bb4d0
Akemi <wakamihirro@gmail.com>
2025-04-25 03:43:20 +0700

    Push

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,50 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text, ImageBackground, ImageBase, Image } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Colors } from '@/app-example/constants/Colors'
+import { blue } from 'react-native-reanimated/lib/typescript/Colors'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                headerShown:false,
+                tabBarActiveBackgroundColor: "#979391",
+                tabBarStyle: {
+                    backgroundColor: '#3d3d3d'
+                },
+                tabBarIcon: ({ focused }) => (
+                    <>
+                        <Image 
+                            source={require('..\\assets\\icons\\home.png')}
+                            tintColor="white"
+                        />
+                    </>
+                )
+            }}
+        />
+        <Tabs.Screen
+            name="library"
+            options={{
+                headerShown:false
+            }}
+        />
+        <Tabs.Screen
+            name="search"
+            options={{
+                headerShown:false
+            }}
+        />
+        <Tabs.Screen
+            name="setting"
+            options={{
+                headerShown:false
+            }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default _layout
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors:{
+        one: "#3d3d3d",
+        two: "#171414",
+        three: "#ff9a68"
+      }
+    },
+  },
+  plugins: [],
+}
